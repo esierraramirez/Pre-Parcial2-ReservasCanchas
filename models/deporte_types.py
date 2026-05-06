ba6ae0d5af562d7414363ca11a3cbc1bc80d7134
esierraramirez <essierra62@ucatolica.edu.co>
2026-05-06 08:21:40 -0500

    Creacion de modelo tipos de deporte

---
+++ models/deporte_types.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Deporte(str, Enum):
+    FUTBOL = "futbol"
+    MICROFUTBOL = "microfutbol"
+    BALONCESTO = "baloncesto"
+    TENIS = "tenis"
+    VOLEIBOL = "voleibol"
